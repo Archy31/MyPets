e56f0534fea878e0023c853f2f461e5d07b81209
kairat <kubanychbekukairat@gmail.com>
2022-09-17 22:16:42 +0600

    Created and added new project in Python and C languages...

---
+++ CodeC/C_files/date_types.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main()
+{
+	printf("My first programm in C language!\n");
+	// "When something is important enough, you do it even if the odds are not in your favor."
+	/*
+	   "There have to be reasons that you get up in the morning and you want to live. 
+	   Why do you want to live? What's the point? What inspires you? What do you love about the future? 
+	   If the future does not include being out there among the stars and being a multi-planet species, 
+	   I find that incredibly depressing."
+	                                       - Elon Musk
+	*/
+}
+
+
+/*
+  void date_types()
+{
+	char one = 'S';
+	short two = 34;
+	int four = 3456;
+	float four = 0.12345;
+	double six = 0.123456789;
+	long eith = 43892243;
+	long long sixteen = 1234567887654321;
+}
+*/
---
+++ CodeC/testing.py
@@ -0,0 +1,3 @@
+from run_C_file import run_C
+
+run_C('date_types', add_path='/C_files')
